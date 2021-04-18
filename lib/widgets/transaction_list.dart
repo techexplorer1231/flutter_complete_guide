805d363975e08cef78f7ec5aabe930ff6b9555cc
Pratap.Krishna.Singh <pratap.singh@nagarro.com>
2021-04-18 15:29:27 +0530

    added bottom sheet

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -13,7 +13,7 @@ class TransactionList extends StatelessWidget {
     return Container(
       height: 300,
       child: ListView.builder(
-        itemBuilder: (context, index) {
+        itemBuilder: (ctx, index) {
           return Card(
             child: Row(
               children: <Widget>[
@@ -30,7 +30,7 @@ class TransactionList extends StatelessWidget {
                   ),
                   padding: EdgeInsets.all(10),
                   child: Text(
-                    '\$${transactions[index].amount}',
+                    '\$${transactions[index].amount.toStringAsFixed(2)}',
                     style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 20,

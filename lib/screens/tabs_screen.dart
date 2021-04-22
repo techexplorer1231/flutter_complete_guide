80f522a58fbfee98e830deaddc123999c021a5e4
Pratap.Krishna.Singh <pratap.singh@nagarro.com>
2021-04-22 22:50:36 +0530

    Ttile changes on bottom tab controller change

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -9,9 +9,9 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-  final List<Widget> _pages = [
-    CategoriesScreen(),
-    FavoritesScreen()
+  final List<Map<String, Object>> _pages = [
+    {'page': CategoriesScreen(), 'title': 'Categories'},
+    {'page': FavoritesScreen(), 'title': 'Favorites'},
   ];
 
   int _selectedPageIndex = 0;
@@ -26,9 +26,9 @@ class _TabsScreenState extends State<TabsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
+        title: Text(_pages[_selectedPageIndex]['title']),
       ),
-      body: _pages[_selectedPageIndex],
+      body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,

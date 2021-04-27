eed9ab29362abc5460fd0b8deeff75559ff472b3
Pratap.Krishna.Singh <pratap.singh@nagarro.com>
2021-04-27 21:13:11 +0530

    initial boilerplate

--- lib/main.dart
+++ lib/main.dart
@@ -1,119 +1,29 @@
 import 'package:flutter/material.dart';
 
-import './dummy_data.dart';
-import 'models/meal.dart';
-import 'screens/categories_screen.dart';
-import 'screens/category_meals_screen.dart';
-import 'screens/filter_screen.dart';
-import 'screens/meal_detail_screen.dart';
-import 'screens/tabs_screen.dart';
-
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  Map<String, bool> _filters = {
-    'gluten': false,
-    'lactose': false,
-    'vegetarian': false,
-    'vegan': false,
-  };
-
-  List<Meal> _availableMeals = DUMMY_MEALS;
-  List<Meal> _favoriteMeals = [];
-
-  void _setFilters(Map<String, bool> filterData) {
-    setState(() {
-      _filters = filterData;
-
-      _availableMeals = DUMMY_MEALS.where((meal) {
-        if (_filters['gluten'] && !meal.isGlutenFree) {
-          return false;
-        }
-        if (_filters['lactose'] && !meal.isLactoseFree) {
-          return false;
-        }
-        if (_filters['vegetarian'] && !meal.isVegetarian) {
-          return false;
-        }
-        if (_filters['vegan'] && !meal.isVegan) {
-          return false;
-        }
-        return true;
-      }).toList();
-    });
-  }
-
-  void _toggleFavorite(String mealId) {
-    final existingIndex = _favoriteMeals.indexWhere((meal) => mealId == meal.id);
-    if(existingIndex >= 0) {
-      setState(() {
-        _favoriteMeals.removeAt(existingIndex);
-      });
-    } else {
-      setState(() {
-        _favoriteMeals.add(DUMMY_MEALS.firstWhere((meal) => meal.id == mealId));
-      });
-    }
-  }
-
-  bool _isMealFavorite(String id) {
-    return _favoriteMeals.any((meal) => meal.id == id);
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'DeliMeals',
+      title: 'MyShop',
       theme: ThemeData(
-          primarySwatch: Colors.pink,
-          accentColor: Colors.amber,
-          canvasColor: Color.fromRGBO(255, 254, 229, 1),
-          fontFamily: 'Raleway',
-          textTheme: ThemeData.light().textTheme.copyWith(
-                body1: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                body2: TextStyle(color: Color.fromRGBO(20, 51, 51, 1)),
-                title: TextStyle(
-                    fontSize: 20,
-                    fontFamily: 'RobotoCondensed',
-                    fontWeight: FontWeight.bold),
-              )),
-      initialRoute: '/',
-      routes: {
-        '/': (ctx) => TabsScreen(_favoriteMeals),
-        CategoryMealsScreen.routeName: (ctx) =>
-            CategoryMealsScreen(_availableMeals),
-        MealDetailScreen.routeName: (ctx) => MealDetailScreen(_toggleFavorite),
-        FilterScreen.routeName: (ctx) => FilterScreen(_filters, _setFilters),
-      },
-      // onGenerateRoute: (settings) {
-      //   return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
-      // },
-      onUnknownRoute: (settings) {
-        return MaterialPageRoute(builder: (ctx) => CategoriesScreen());
-      },
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DeliMeals'),
+        title: Text('MyShop'),
       ),
       body: Center(
-        child: Text('Navigation Time!'),
+        child: Text('Let\'s build a shop!'),
       ),
     );
   }
